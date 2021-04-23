2f4f9069a8b799b4645c9d712a0ac4a4ee455efb
spiderweb0802 <81157779+bkrcap@users.noreply.github.com>
2021-04-23 21:48:36 +0900

    with_optionを用い、ネスト構造で共通バリデーションをまとめるように修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,18 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい" }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい" }
-  validates :last_name_kana, presence: true, format: {
-    with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,
-    message: "全角カタカナのみで入力して下さい"
-  }
-  validates :first_name_kana, presence: true, format: {
-    with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,
-    message: "全角カタカナのみで入力して下さい"
-  }
-  validates :birthday, presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :birthday
+
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥々ー]+\z/, message: "は全角ひらがな、全角カタカナ、漢字で入力して下さい" }do
+      validates :last_name
+      validates :first_name
+    end
+
+    with_options format: { with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: "全角カタカナのみで入力して下さい" } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+  end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
