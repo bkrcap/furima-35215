0cf4009b81d5b4010985c3d79837d14de0b6eb5b
spiderweb0802 <81157779+bkrcap@users.noreply.github.com>
2021-04-25 20:19:32 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,12 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  private
+
+  def item_params 
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
 end
+
+#params.require(:モデル).permit(:左のモデルの中でも、保存したい値を制限したい時に、permitでカラム指定).merge(結合させたい外部キー。商品を出品したユーザーの情報を紐付けたい時)
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase_management
+  has_one_attached :image
 end
