b05d71083295f6a94d8ad8a1c43986c5c21ded98
spiderweb0802 <81157779+bkrcap@users.noreply.github.com>
2021-04-26 15:24:54 +0900

    バリデーション（仮）

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,18 @@ class Item < ApplicationRecord
   belongs_to :shipping_charge
   belongs_to :shipping_area
   belongs_to :shipping_days
+
+  with_options presence: true do
+    validates :item_name
+    validates :description_item
+    validates :selling_price, format: { with: /\A[0-9]+\z/, message: "半角数字のみで入力して下さい" }
+
+    with_options numericality: { other_than: 1 }do
+      validates :category_id
+      validates :item_status_id
+      validates :shipping_charge_id
+      validates :shipping_area_id
+      validates :shipping_days_id
+    end
+  end
 end
