5ae65fa4e4639a0120fa7df438fed916d5aeafa8
spiderweb0802 <81157779+bkrcap@users.noreply.github.com>
2021-04-26 14:59:45 +0900

    アソシエーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase_management
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :shipping_charge
+  belongs_to :shipping_area
+  belongs_to :shipping_days
 end
