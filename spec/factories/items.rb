a715867db3b83df100cffbec6742844831b004e1
spiderweb0802 <81157779+bkrcap@users.noreply.github.com>
2021-04-27 18:10:57 +0900

    数値のためクォーテーションを外す

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     item_name          {'商品名'}
     description_item   {'商品説明'}
-    category_id        {'2'}
-    item_status_id     {'2'}
-    shipping_charge_id {"2"}
-    shipping_area_id   {"2"}
-    shipping_days_id   {"2"}
-    selling_price      {"5000"}
+    category_id        {2}
+    item_status_id     {2}
+    shipping_charge_id {2}
+    shipping_area_id   {2}
+    shipping_days_id   {2}
+    selling_price      {5000}
     association :user
 
     after(:build) do |item|
