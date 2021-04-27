5503e4065c37df317a1087ef1814653a085aec5d
spiderweb0802 <81157779+bkrcap@users.noreply.github.com>
2021-04-27 18:11:50 +0900

    不要なコメントアウト削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,5 +23,3 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:selling_price, :image, :item_name, :description_item, :category_id, :item_status_id, :shipping_charge_id, :shipping_area_id, :shipping_days_id).merge(user_id: current_user.id)
   end
 end
-
-#params.require(:モデル).permit(:左のモデルの中でも、保存したい値を制限したい時に、permitでカラム指定).merge(結合させたい外部キー。商品を出品したユーザーの情報を紐付けたい時)
