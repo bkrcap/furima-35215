0acb19a93667492d21e24994efe3acf0f5418c4c
spiderweb0802 <81157779+bkrcap@users.noreply.github.com>
2021-05-03 12:11:02 +0900

    purchase_managementsテーブルとのアソシエーションが必要なため記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
-  #一旦コメントアウト。実装時にコメントアウト外すため。has_many :purchase_managements
+  has_many :purchase_managements
 
   with_options presence: true do
     validates :nickname
